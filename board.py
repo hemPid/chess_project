d65dc1c4dfa5a1d541645546fe85ab746f889083
hemPid <yrchikovivan@gmail.com>
2022-11-27 15:18:14 +0300

    board class added

---
+++ board.py
@@ -0,0 +1,23 @@
+class Board:
+    """docstring for Board"""
+    def __init__(self):
+        self.figs = {"white": {
+                     "p1": "a2",
+                     "p2": "b2",
+                     "p3": "c2",
+                     "p4": "d2",
+                     "p5": "e2",
+                     "p6": "f2",
+                     "p7": "g2",
+                     "p8": "h2"
+                     },
+                     "black": {
+                     "p1": "a2",
+                     "p2": "b2",
+                     "p3": "c2",
+                     "p4": "d2",
+                     "p5": "e2",
+                     "p6": "f2",
+                     "p7": "g2",
+                     "p8": "h2"
+                     }}
