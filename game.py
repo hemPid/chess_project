646989f2d9ac75afefdc2a9af134a1fdf97a64e1
hemPid <yrchikovivan@gmail.com>
2022-11-30 13:27:15 +0300

    drawing figs added

--- game.py
+++ game.py
@@ -15,9 +15,10 @@ class Game:
         self.tc_add = tc_add
         self.w_time = tc_start
         self.b_time = tc_start
-        self.board = board.Board()
+        self.bd = board.Board()
 
     def draw_board(self, pos, cell_size, side='w'):
+        #drawing cells
         for i in range(8):
             for j in range(8):
                 color = None
@@ -29,3 +30,19 @@ class Game:
                                  (pos[0] + cell_size*j,
                                   pos[1] + cell_size*i,
                                   cell_size, cell_size))
+        #drawing figures
+        for sd in self.bd.figs:
+            for fig in self.bd.figs[sd]:
+                fig_surf = pygame.image.load('models\\' + sd + '_' + fig[0] + '.png').convert_alpha()
+                fig_im = pygame.transform.scale(fig_surf, (cell_size, cell_size))
+                fig_pos_x = 0
+                fig_pos_y = 0
+                fig_coord_x = self.bd.figs[sd][fig][0]
+                fig_coord_y = int(self.bd.figs[sd][fig][1])
+                if side == 'w':
+                    fig_pos_x = (ord(fig_coord_x) - ord('a')) * cell_size + pos[0]
+                    fig_pos_y = (8 - fig_coord_y) * cell_size + pos[1]
+                else:
+                    fig_pos_x = (ord('h') - ord(fig_coord_x)) * cell_size + pos[0]
+                    fig_pos_y = (fig_coord_y - 1) * cell_size + pos[1]
+                self.screen.blit(fig_im, (fig_pos_x, fig_pos_y))
