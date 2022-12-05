b1e6571abd89320e9762b499d0bf09bf91b1d402
hemPid <yrchikovivan@gmail.com>
2022-12-05 16:12:42 +0300

    flake8 fixed on game type choice

--- game_type_choice.py
+++ game_type_choice.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 class game_type_choice_window:
     """docstring for game_type_choice_window"""
     def __init__(self, screen):
@@ -9,19 +10,21 @@ class game_type_choice_window:
         # rendering header
         self.header_font = pygame.font.SysFont('arial', 40, True)
         header_text = "Choose game type:"
-        self.header_rendered = self.header_font.render(header_text, True, (0, 0, 0))
+        self.header_rendered = self.\
+            header_font.render(header_text, True, (0, 0, 0))
         # rendering icon text
         self.icon_font = pygame.font.SysFont('arial', 20, True)
         single_player_icon_text = "single board"
         multiplayer_icon_text = "online game"
-        self.single_rendered_text = self.icon_font.render(single_player_icon_text, True, (0, 0, 0))
-        self.multiplayer_rendered_text = self.icon_font.render(multiplayer_icon_text, True, (0, 0, 0))
+        self.single_rendered_text = self.icon_font.\
+            render(single_player_icon_text, True, (0, 0, 0))
+        self.multiplayer_rendered_text = self.icon_font.\
+            render(multiplayer_icon_text, True, (0, 0, 0))
         # initial drawing
         self.draw_initial_choice()
 
     def loop(self, dt):
         return dt
-
 
     def ev(self, events, dt):
         for event in events:
@@ -29,7 +32,6 @@ class game_type_choice_window:
                 for but in self.buttons:
                     if but['rect'].collidepoint(event.pos):
                         print(but['name'])
-                        
 
     def draw_initial_choice(self):
         self.screen.fill((255, 255, 255))
@@ -50,12 +52,17 @@ class game_type_choice_window:
         height = 200
         icon_size = 150
         color = (243, 194, 105)
-        rect = pygame.draw.rect(self.screen, color, (pos_x, pos_y, width, height))
+        rect = pygame.draw.\
+            rect(self.screen, color, (pos_x, pos_y, width, height))
         self.buttons.append({'name': 'single', 'rect': rect})
         icon_im = pygame.image.load('icons\\moni_icon.png').convert_alpha()
-        icon_transformed = pygame.transform.scale(icon_im, (icon_size, icon_size))
-        self.screen.blit(icon_transformed, (pos_x + (width // 2) - (icon_size // 2), pos_y + 10))
-        self.screen.blit(self.single_rendered_text, (pos_x + 90, pos_y + height - 30))
+        icon_transformed = pygame.transform.\
+            scale(icon_im, (icon_size, icon_size))
+        self.screen.blit(icon_transformed,
+                         (pos_x + (width // 2) - (icon_size // 2),
+                          pos_y + 10))
+        self.screen.blit(self.single_rendered_text,
+                         (pos_x + 90, pos_y + height - 30))
 
     def draw_multiplayer_button(self):
         pos_x = 340
@@ -64,11 +71,17 @@ class game_type_choice_window:
         height = 200
         icon_size = 100
         color = (243, 194, 105)
-        rect = pygame.draw.rect(self.screen, color, (pos_x, pos_y, width, height))
+        rect = pygame.draw.rect(self.screen, color,
+                                (pos_x, pos_y, width, height))
         self.buttons.append({'name': 'multi', 'rect': rect})
         icon_im = pygame.image.load('icons\\moni_icon.png').convert_alpha()
-        icon_transformed = pygame.transform.scale(icon_im, (icon_size, icon_size))
-        self.screen.blit(icon_transformed, (pos_x + (width - 2*icon_size)//2, pos_y + 10))
-        self.screen.blit(icon_transformed, (pos_x + (width - 2*icon_size)//2 + icon_size, pos_y + 10))
-        self.screen.blit(self.multiplayer_rendered_text, (pos_x + 100, pos_y + height - 30))
-
+        icon_transformed = pygame.transform.\
+            scale(icon_im, (icon_size, icon_size))
+        self.screen.blit(icon_transformed,
+                         (pos_x + (width - 2*icon_size)//2,
+                          pos_y + 10))
+        self.screen.blit(icon_transformed,
+                         (pos_x + (width - 2*icon_size)//2 + icon_size,
+                          pos_y + 10))
+        self.screen.blit(self.multiplayer_rendered_text,
+                         (pos_x + 100, pos_y + height - 30))
