ad109048b91deec87220907147228f4c4d850887
hemPid <yrchikovivan@gmail.com>
2022-12-05 19:46:50 +0300

    field select added

--- single_game.py
+++ single_game.py
@@ -11,12 +11,19 @@ class single_game_window:
         self.next_stage = None
         self.white_color = 0xf0d9b5
         self.black_color = 0xb58863
+        self.board_pos_x = 50
+        self.board_pos_y = 50
+        self.cell_size = 80
         self.bd = board.Board()
         self.current_move = 'w'
 
     def loop(self, dt):
         self.screen.fill((255, 255, 255))
-        self.draw_board((50,50), 80, self.current_move)
+        self.draw_board((self.board_pos_x, self.board_pos_y), self.cell_size, self.current_move)
+        self.select_field('e4')
+        self.select_field('e5')
+        self.select_field('e2')
+        self.select_field('e7')
 
     def ev(self, events, dt):
         pass
@@ -56,3 +63,22 @@ class single_game_window:
                                 * cell_size + pos[0]
                     fig_pos_y = (fig_crd_y - 1) * cell_size + pos[1]
                 self.screen.blit(fig_im, (fig_pos_x, fig_pos_y))
+
+    def get_field_coords(self, field):
+        pos = field
+        coord_x = 0
+        coord_y = 0
+        if self.current_move == 'w':
+            coord_x = self.board_pos_x + self.cell_size*(ord(pos[0]) - ord('a'))
+            coord_y = self.board_pos_y + self.cell_size*(8 - int(pos[1]))
+        else:
+            coord_x = self.board_pos_x + self.cell_size*(ord('h') - ord(pos[0]))
+            coord_y = self.board_pos_y + self.cell_size*(int(pos[1]) - 1)
+        return (coord_x, coord_y)
+
+    def select_field(self, field):
+        cell_pos = self.get_field_coords(field)
+        surf = pygame.Surface((self.cell_size, self.cell_size))
+        surf.set_alpha(90)
+        pygame.draw.circle(surf, (0, 255, 0), (self.cell_size // 2, self.cell_size // 2), self.cell_size*0.2)
+        self.screen.blit(surf, cell_pos)
