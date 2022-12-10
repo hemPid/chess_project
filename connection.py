54f42e04b890ea8422def890ab96b07ab0bfb3be
hemPid <yrchikovivan@gmail.com>
2022-12-10 11:57:22 +0300

    intro message added

--- connection.py
+++ connection.py
@@ -4,9 +4,10 @@ import pubnub.callbacks as pcalls
 
 class Connection:
     """docstring for Connection"""
-    def __init__(self, channel, name, msg_event):
+    def __init__(self, channel, name, msg_event, conn_event):
         self.channel_name = channel
         self.msg_event = msg_event
+        self.conn_event = conn_event
         self.config = pconf.PNConfiguration()
         self.config.subscribe_key = "sub-c-182b6c90-9414-497f-b25c-54129a5236f6"
         self.config.publish_key = "pub-c-5a754c49-a2b9-4cee-ab35-4e840cd37fdc"
@@ -21,8 +22,9 @@ class Connection:
 
     def connect(self):
         class MySubscribeCallback(pcalls.SubscribeCallback):
-            def __init__(self, msg):
-                self.msg = msg
+            def __init__(self, msg_ev, conn_ev):
+                self.msg = msg_ev
+                self.conn = conn_ev
 
             def message(self, pubnub, message):
                 # Handle new message stored in message.message
@@ -33,7 +35,8 @@ class Connection:
                     print('error')
                 else:
                     print('OK')
-        self.listener = MySubscribeCallback(self.msg_event)
+                    self.conn()
+        self.listener = MySubscribeCallback(self.msg_event, self.conn_event)
         self.pubnub.add_listener(self.listener)
         self.pubnub.subscribe().\
             channels(self.channel_name).execute()
--- loading_room.py
+++ loading_room.py
@@ -1,17 +1,50 @@
 import pygame
+import game_type_choice
 import connection
 
 class waiting_room_window:
-	"""docstring for witing_room_window"""
-	def __init__(self, screen, data):
-		self.screen = screen
-		self.data = data
-		self.finished = False
-		self.next_stage = None
-		self.screen.fill((255,255,255))
+    """docstring for witing_room_window"""
+    def __init__(self, screen, data):
+        self.screen = screen
+        self.data = data
+        self.finished = False
+        self.next_stage = None
+        self.screen.fill((255, 255, 255))
+        # drawing text and quit button
+        self.button_font = pygame.font.SysFont('arial', 40, True)
+        self.text_font = pygame.font.SysFont('arial', 50, True)
+        self.text_rend = self.text_font.\
+            render('waiting for someone to connect', True, (0, 0, 0))
+        self.screen.blit(self.text_rend, (100, 100))
+        self.but_text_rend = self.button_font.\
+            render('quit', True, (0, 0, 0))
+        self.buttons_color = (243, 194, 105)
+        self.quit_but = pygame.draw.\
+            rect(self.screen, self.buttons_color,
+                 (100, 200,
+                  self.but_text_rend.get_width() + 10,
+                  self.but_text_rend.get_height() + 10))
+        self.screen.blit(self.but_text_rend, (105, 205))
+        # forming connection
+        self.con = connection.Connection('waiting_room', self.data['name'], self.msg_listener, self.introduce)
+        self.con.connect()
+        self.data_to_send = self.data.copy()
+        self.data_to_send['msg_type'] = 'introduction'
 
-	def loop(self, dt):
-		pass
+    def loop(self, dt):
+        pass
 
-	def ev(self, events, dt):
-		pass
+    def ev(self, events, dt):
+        for event in events:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if self.quit_but.collidepoint(event.pos):
+                    self.con.disconnect()
+                    self.finished = True
+                    self.data = {'name': self.data['name']}
+                    self.next_stage = game_type_choice.game_type_choice_window
+
+    def introduce(self):
+        self.con.write(self.data_to_send)
+
+    def msg_listener(self, message):
+        print(message.message)
