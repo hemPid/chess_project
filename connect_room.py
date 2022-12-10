783d7f9a8c68c775b04b06edf69bba9aad1184f7
hemPid <yrchikovivan@gmail.com>
2022-12-10 12:47:13 +0300

    remove action added

--- connect_room.py
+++ connect_room.py
@@ -8,7 +8,9 @@ class connect_window:
 		self.finished = False
 		self.next_stage = None
 		self.screen.fill((255, 255, 255))
+		self.aviliable_rooms = {}
 		self.buttons = []
+		self.buttons_color = (243, 194, 105)
 		self.con = connection.Connection('waiting_room', self.data['name'], self.msg_listener, self.request)
 		self.con.connect()
 
@@ -21,11 +23,19 @@ class connect_window:
 	def draw_list(self):
 		pass
 
+	def get_button_coords(self):
+		return (100, 100, 100, 100)
+
 	def request(self):
 		self.con.write({'msg_type': 'request'})
 
 	def msg_listener(self, message):
 		if message.message['msg_type'] == 'introduction':
-			print(message.message)
+			self.aviliable_rooms[message.message['name']] = {
+				'tc': message.message['tc'],
+				'button': pygame.draw.rect(self.screen, self.buttons_color, self.get_button_coords())
+			}
 		elif message.message['msg_type'] == 'remove':
-			print(message.message)
+			self.aviliable_rooms.pop(message.message['name'])
+		print(message.message)
+		print(self.aviliable_rooms)
